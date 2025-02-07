f2dbbe8b46375a670907ad421d0a0a7e68728d30
Caio Vinicius <caioviniaires@gmail.com>
2025-02-06 21:59:06 -0300

    Update

---
+++ Exemplo3.py
@@ -0,0 +1,4 @@
+# Faça um programa que receba um salário de R$ 1500 e tenha um aumento de 5% do salário e ao final exiba o salário com aumento
+salario = 1500
+aumento = salario *0.05
+print(aumento + salario) 
