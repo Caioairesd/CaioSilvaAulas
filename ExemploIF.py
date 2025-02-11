e33aea55951e9489495069e30b1e662831981c57
Caio Vinicius <caioviniaires@gmail.com>
2025-02-10 22:16:21 -0300

    update

---
+++ ExemploIF.py
@@ -0,0 +1,11 @@
+num = 11
+if num > 10:
+    print("O número é maior que 10")
+elif num == 10:
+        print("O número é igual a 10")
+else: 
+    print("O número é menor que 10")
+
+
+        
+        
