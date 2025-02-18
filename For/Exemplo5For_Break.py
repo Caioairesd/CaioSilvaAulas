d952655661fb7f74041b5f02a8c7eaa80c2c8056
Caio Vinicius <caioviniaires@gmail.com>
2025-02-17 22:09:46 -0300

    ajuste

--- For/Exemplo5For_Break.py
+++ For/Exemplo5For_Break.py
@@ -6,5 +6,7 @@ for pessoa in PessoasPresentes:
     if pessoa == chamada:
         print("{} Está presente".format(chamada))
         break
-else:
-    print("Só um print para mostrar que o for já passou por essa pessoa:"+str(pessoa))
+    else:
+        print("Só um print para mostrar que o for já passou por essa pessoa:"+str(pessoa))
+        continue
+        print("Só para mostrar que não aparece")
