6f9fe177474260ee8dfe84c08f949565437efc12
Caio Vinicius <caioviniaires@gmail.com>
2025-02-17 22:11:24 -0300

    ajuste

--- For/Exemplo2For_IF.py
+++ For/Exemplo2For_IF.py
@@ -6,4 +6,4 @@ for venda in vendas:
     if venda >= meta:
         qtde_bateu_meta +=1
 qtde_funcionarios = len(vendas)
-print("O percentual de pessoas que bateram  a meta foi de {:.3%}".format(qtde_bateu_meta/qtde_funcionarios))
+print("O percentual de pessoas que bateram  a meta foi de {:.1%}".format(qtde_bateu_meta/qtde_funcionarios))
