db4d791888f582fd23847ac52a95f7e692d31fed
Caio Vinicius <caioviniaires@gmail.com>
2025-02-19 21:45:12 -0300

    Organização e adição
    Adição de tupla e organização,

---
+++ Tuplas/Ex01_Tupla.py
@@ -0,0 +1,13 @@
+vendas = ('Caio','27/08/2024','30/01/2008','16789','Dev Sênior',)
+nome,dataContratacao,dataDeNascimento,salario,cargo = vendas
+print(nome)
+print(dataContratacao)
+print(dataDeNascimento)
+print(salario)
+print(cargo)
+
+
+
+
+
+
